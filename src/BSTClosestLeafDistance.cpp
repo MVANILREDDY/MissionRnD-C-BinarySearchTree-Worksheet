017b5faa39b2565e21cff26ea7ccda0fb66c27aa
MVANILREDDY <MVANILREDDY@missionrnd.com>
2016-04-26 23:59:35 +0530

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -38,8 +38,75 @@ struct node{
   int data;
   struct node *right;
 };
+void finding_leaf_nodes(struct node *, struct  node *, struct node *, int * );
+void root_distance(struct node *, struct node *, int, int *);
+int least_distance(struct node *, struct node * , struct node *);
+struct node * common_root(struct node * root, struct node * leaf, struct node * temp);
 
 int get_closest_leaf_distance(struct node *root, struct node *temp)
 {
-  return -1;
+	if (root == NULL || temp == NULL)
+		return -1;
+	else
+	{
+		int least = -1;
+		finding_leaf_nodes(root, root, temp, &least);
+		return least;
+	}
+}
+void root_distance(node * node1, node * node2, int count, int * r){
+	if (node1 == NULL || node2 == NULL)
+		return;
+	if (node1 == node2){
+		*r = count;
+		return;
+	}
+	else if (node1->data < node2->data){
+		root_distance(node1->right, node2, count + 1, r);
+	}
+	else if (node1->data > node2->data){
+		root_distance(node1->left, node2, count + 1, r);
+	}
+}
+int least_distance(node * root, node * node1, node * node2){
+	int x = 0, y = 0;
+	node * sub = common_root(root, node1, node2);
+	root_distance(sub, node1, 0, &x);
+	root_distance(sub, node2, 0, &y);
+	return  (x + y);
+}
+
+void  finding_leaf_nodes(struct node * org_root, struct  node * root, struct node * temp, int * r){
+	if (root == NULL)
+		return;
+	if (root->left == NULL&&root->right == NULL){
+		int distance = least_distance(org_root, root, temp);
+		if (*r == -1)
+			*r = distance;
+		else if (distance < *r)
+			*r = distance;
+		return;
+	}
+	else{
+		finding_leaf_nodes(org_root, root->left, temp, r);
+		finding_leaf_nodes(org_root, root->right, temp, r);
+		return;
+	}
+}
+struct node * common_root(node * root, node * leaf, node * temp)
+{
+	if (leaf == NULL || temp == NULL)
+		return NULL;
+	else{
+		if (leaf->data < root->data && temp->data < root->data){
+			return common_root(root->left, leaf, temp);
+		}
+		else if (leaf->data > root->data && temp->data > root->data){
+			return common_root(root->right, leaf, temp);
+		}
+		else
+		{
+			return root;
+		}
+	}
 }
